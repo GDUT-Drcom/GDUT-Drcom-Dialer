73fbc3f60f299e8bcd4c807356c27a7021413970
Jiangming1399 <Jiangming1399@163.com>
2016-11-18 21:44:21 +0800

    增加PPPoE拨号类

---
+++ Drcom-Dialer/Model/PPPoE.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DotRas;
+
+namespace Drcom_Dialer.Model
+{
+    /// <summary>
+    /// PPPoE拨号器
+    /// </summary>
+    class PPPoE
+    {
+        public PPPoE()
+        {
+            init(Properties.Resources.RasConnectionName);
+        }
+
+        /// <summary>
+        /// 初始化PPPoE拨号器
+        /// </summary>
+        /// <param name="connectName">连接名称</param>
+        private void init(string connectName )
+        {
+            try
+            {
+                RasDialer dialer = new RasDialer();
+                RasPhoneBook phoneBook = new RasPhoneBook();
+                string path = RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User);
+                phoneBook.Open(path);
+
+                if (phoneBook.Entries.Contains(connectName))
+                {
+                    phoneBook.Entries[connectName].PhoneNumber = " ";
+                    phoneBook.Entries[connectName].Update();
+                }
+                else
+                {
+                    string adds = string.Empty;
+                    System.Collections.ObjectModel.ReadOnlyCollection<RasDevice> readOnlyCollection = RasDevice.GetDevices();
+                    RasDevice device = RasDevice.GetDevices().Where(o => o.DeviceType == RasDeviceType.PPPoE).First();
+                    RasEntry entry = RasEntry.CreateBroadbandEntry(connectName, device);
+                    entry.PhoneNumber = " ";
+                    phoneBook.Entries.Add(entry);
+                }
+            }
+            catch (Exception e)
+            {
+                Utils.Log4Net.WriteLog(e.Message, e);
+            }
+
+        }
+
+        /// <summary>
+        /// 拨号
+        /// </summary>
+        /// <param name="username">用户名</param>
+        /// <param name="password">密码</param>
+        public void Dial(string username, string password)
+        {
+            Dial(username, password, Properties.Resources.RasConnectionName);
+        }
+        /// <summary>
+        /// 拨号
+        /// </summary>
+        /// <param name="username">用户名</param>
+        /// <param name="password">密码</param>
+        /// <param name="connectName">连接名</param>
+        public void Dial(string username, string password, string connectName)
+        {
+            try
+            {
+                RasDialer dialer = new RasDialer();
+                dialer.EntryName = connectName;
+                dialer.PhoneNumber = " ";
+                dialer.AllowUseStoredCredentials = true;
+                dialer.PhoneBookPath = RasPhoneBook.GetPhoneBookPath(RasPhoneBookType.User);
+                dialer.Credentials = new System.Net.NetworkCredential(username, password);
+                dialer.Timeout = 1000;
+
+                RasHandle hRas = dialer.Dial();
+
+                while (hRas.IsInvalid)
+                {
+                    //TODO: Add code here
+                }
+                if (!hRas.IsInvalid)
+                {
+                    foreach (RasConnection conn in RasConnection.GetActiveConnections())
+                    {
+                        if (conn.Handle == hRas)
+                        {
+                            RasIPInfo ipAddr = (RasIPInfo)conn.GetProjectionInfo(RasProjectionType.IP);
+                            //ipAddr.IPAddress.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Utils.Log4Net.WriteLog(e.Message, e);
+            }
+        }
+        /// <summary>
+        /// 断开所有连接
+        /// </summary>
+        public void Hangup()
+        {
+            try
+            {
+                foreach (RasConnection conn in RasConnection.GetActiveConnections())
+                {
+                    conn.HangUp();
+                }
+            }
+            catch (Exception e)
+            {
+                Utils.Log4Net.WriteLog(e.Message, e);
+            }
+        }
+    }
+}
