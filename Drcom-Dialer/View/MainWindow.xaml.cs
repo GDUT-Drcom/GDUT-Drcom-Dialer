cb782e67c08c1059c36819a9a6e255e0a9cde283
Jiangming1399 <Jiangming1399@163.com>
2016-11-18 13:22:55 +0800

    添加主窗口

---
+++ Drcom-Dialer/View/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Drcom_Dialer.View
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_dial_Click(object sender, RoutedEventArgs e)
+        {
+            //TODO:add code here
+        }
+
+        private void cb_autoLogin_Checked(object sender, RoutedEventArgs e)
+        {
+            //TODO:add code here
+        }
+
+        private void cb_remember_Checked(object sender, RoutedEventArgs e)
+        {
+            //TODO:add code here
+        }
+    }
+}
