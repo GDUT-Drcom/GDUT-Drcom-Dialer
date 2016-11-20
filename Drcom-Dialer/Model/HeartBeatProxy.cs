63de79ed9067c36f289761c82244b48da2eedc85
Jiangming1399 <Jiangming1399@163.com>
2016-11-20 10:38:35 +0800

    新增心跳包代理类

---
+++ Drcom-Dialer/Model/HeartBeatProxy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drcom_Dialer.Model
+{
+    /// <summary>
+    /// 心跳包代理类
+    /// </summary>
+    static class HeartBeatProxy
+    {
+        /// <summary>
+        /// 初始化心跳包
+        /// </summary>
+        public static status init()
+        {
+            return status.Success;
+        }
+        /// <summary>
+        /// 进行心跳操作
+        /// </summary>
+        public static status heartbeat()
+        {
+            return status.Success;
+        }
+        /// <summary>
+        /// 反初始化
+        /// </summary>
+        public static status deinit()
+        {
+            return status.Success;
+        }
+
+        public enum status
+        {
+            Success = 0,
+            BindPortFail = -1,
+            PermissonDeny = -2,
+            RecvTimedOut = -3,
+            Unknown = -99
+        }
+    }
+}
