ad6c8f6e456edc8eca333cdb70f5f2a804428775
Wingkou Loeng <961574992@qq.com>
2016-12-06 18:35:26 +0800

    ViewModel添加了对RememberPassword和AutoLogin的设置。测试下载dll中。添加了一些log。设置窗体icon。

--- Drcom-Dialer/Model/Utils/HeartBeatUpdate.cs
+++ Drcom-Dialer/Model/Utils/HeartBeatUpdate.cs
@@ -27,10 +27,10 @@ namespace Drcom_Dialer.Model.Utils
             RestRequest request = new RestRequest("/repos/chenhaowen01/gdut-drcom/releases/latest");
             IRestResponse<GithubReleaseResponse> response = client.Execute<GithubReleaseResponse>(request);
 
-            if(response != null && response.Content !="" 
-                && response.ResponseStatus == ResponseStatus.Completed)
+            if (response != null &&
+                response.Content != "" &&
+                response.ResponseStatus == ResponseStatus.Completed)
             {
-                //简化代码
                 return response.Data.name != GDUT_Drcom.Version;
             }
 
@@ -39,34 +39,38 @@ namespace Drcom_Dialer.Model.Utils
             request = new RestRequest("/repos/chenhaowen01/gdut-drcom/releases/latest");
             response = client.Execute<GithubReleaseResponse>(request);
 
-            if (response != null && response.Content != "" 
-                && response.ResponseStatus == ResponseStatus.Completed)
+            if (response != null &&
+                response.Content != "" &&
+                response.ResponseStatus == ResponseStatus.Completed)
             {
-                //简化
                 return response.Data.name != GDUT_Drcom.Version;
             }
+
             return false;
         }
+
         /// <summary>
         /// 升级DLL
         /// </summary>
         /// <returns></returns>
         public static bool Update()
         {
+            Log4Net.WriteLog("开始更新gdut-drcom.dll");
+
             RestClient client = new RestClient("https://api.github.com");
+            client.Timeout = 2000;
             RestRequest request = new RestRequest("/repos/chenhaowen01/gdut-drcom/releases/latest");
             IRestResponse<GithubReleaseResponse> response = client.Execute<GithubReleaseResponse>(request);
-
-            //打个回车，太长了
-            if (response != null && response.Content != "" 
-                && response.ResponseStatus == ResponseStatus.Completed)
+            
+            if (response != null &&
+                response.Content != "" &&
+                response.ResponseStatus == ResponseStatus.Completed)
             {
-                if(response.Data.assets != null)
+                if (response.Data.assets != null)
                 {
-                    //还是加括号
-                    foreach(GithubReleaseAssetItem fileName in response.Data.assets)
+                    foreach (GithubReleaseAssetItem fileName in response.Data.assets)
                     {
-                        if(fileName.name == "gdut-drcom.dll")
+                        if (fileName.name == "gdut-drcom.dll")
                         {
                             if (DownloadFile(fileName.browser_download_url, "gdut-drcom.dll"))
                             {
@@ -79,15 +83,21 @@ namespace Drcom_Dialer.Model.Utils
 
             //mirror
             client = new RestClient("https://api.github.com");
-            request = new RestRequest("/repos/chenhaowen01/gdut-drcom/releases/latest");
+            // todo:
+            // 临时测试,注意在发行的时候改正
+            request = new RestRequest("/repos/chenhaowen01/gdut-drcom/git/trees/abd65a02a4cb2cabcb40faff635744647daf57a7");
             response = client.Execute<GithubReleaseResponse>(request);
 
-            if (response != null && response.Content != "" && response.ResponseStatus == ResponseStatus.Completed)
+            if (response != null &&
+                response.Content != "" &&
+                response.ResponseStatus == ResponseStatus.Completed)
             {
                 if (response.Data.assets != null)
                     foreach (GithubReleaseAssetItem fileName in response.Data.assets)
                     {
-                        if (fileName.name == "gdut-drcom.dll")
+                        // todo:
+                        // 临时测试,注意在发行的时候改正
+                        if (fileName.name == "gdut-drcom.dll.1.6.3")
                         {
                             if (DownloadFile(fileName.browser_download_url, "gdut-drcom.dll"))
                             {
@@ -99,35 +109,37 @@ namespace Drcom_Dialer.Model.Utils
 
             return false;
         }
+
         /// <summary>
         /// 下载文件
         /// </summary>
         /// <param name="url">链接</param>
         /// <param name="path">本地文件</param>
         /// <returns></returns>
-        private static bool DownloadFile(string url,string path)
+        private static bool DownloadFile(string url, string path)
         {
             int index = url.IndexOf("/", 10); //懒的用其他的了，这是第三个/的出现的位置
 
-            RestClient client = new RestClient(url.Substring(0,index));
-            RestRequest request = new RestRequest(url.Substring(index,url.Length - index));
+            RestClient client = new RestClient(url.Substring(0, index));
+            RestRequest request = new RestRequest(url.Substring(index, url.Length - index));
 
             byte[] result = client.DownloadData(request);
             if (result.Length < 1024)
             {
+                Log4Net.WriteLog($"下载心跳包失败: 太少数据({result.Length}b)");
                 return false;
             }
             try
             {
                 //换用using
-                using (FileStream stream=new FileStream(path,FileMode.Create))
+                using (FileStream stream = new FileStream(path, FileMode.Create))
                 {
                     stream.Write(result, 0, result.Length);
                 }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
-                Log4Net.WriteLog(e.Message, e);
+                Log4Net.WriteLog("下载心跳包失败: " + e.Message, e);
                 return false;
             }
 
