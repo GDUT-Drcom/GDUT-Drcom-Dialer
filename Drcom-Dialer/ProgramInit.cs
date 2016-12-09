e1e5ed910ac39ea6d1183038c355df77c46cff27
Jiangming1399 <Jiangming1399@163.com>
2016-12-10 00:10:48 +0800

    修正启动顺序&&添加注释

--- Drcom-Dialer/ProgramInit.cs
+++ Drcom-Dialer/ProgramInit.cs
@@ -18,23 +18,12 @@ namespace Drcom_Dialer
         [STAThread]
         private static void Main(string[] args)
         {
-            //更新
-            if (Update())
-                return;
-
-            //防止多启
-            string mutexName = Properties.Resources.ProgramTitle + "Mutex";
-            singleInstanceWatcher = new Mutex(false, mutexName, out createdNew);
-            if (!createdNew)
-            {
-                MessageBox.Show("程序已经运行!", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
-                Environment.Exit(-1);
-            }
-
-            //进行必要的初始化工作
-            Model.Utils.GDUT_Drcom.Load();
+            //基本上是程序的根基，放在最前面初始化
             Model.Utils.Log4Net.SetConfig();
             Model.DialerConfig.Init();
+
+            //解析启动参数
+            //不把更新放在这个之前也是因为考虑到可能使用带参启动的问题
             if (args.Length > 0)
             {
                 switch (args[0])
@@ -48,19 +37,43 @@ namespace Drcom_Dialer
                 }
             }
 
+            //防止多启
+            //在修复VPN的情况下，有可能需要启动两个实例
+            //所以将这个放后面
+            string mutexName = Properties.Resources.ProgramTitle + "Mutex";
+            singleInstanceWatcher = new Mutex(false, mutexName, out createdNew);
+            if (!createdNew)
+            {
+                MessageBox.Show("程序已经运行!", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                Environment.Exit(-1);
+            }
+
+            //更新
+            if (Update())
+                return;
+
+
+            //修复VPN
             if (Model.DialerConfig.isFixVPN)
             {
                 Model.Utils.VPNFixer.Elevate();
             }
 
+            //初始化必要组件
             Model.PPPoE.Init();
             Model.Dial.Init();
+            Model.Utils.GDUT_Drcom.Load();
 
+            //初始化界面
             App app = new App();
             app.InitializeComponent();
             app.Run();
         }
 
+        /// <summary>
+        /// 更新程序文件
+        /// </summary>
+        /// <returns>是否需要重启</returns>
         private static bool Update()
         {
             string name = AppDomain.CurrentDomain.FriendlyName;
@@ -72,15 +85,15 @@ namespace Drcom_Dialer
                 // Clean up
                 if (File.Exists(Model.Utils.Updater.OldName + ".exe"))
                 {
-                    MessageBox.Show("Delete " + Model.Utils.Updater.OldName + ".exe");
+                    Model.Utils.Log4Net.WriteLog("删除旧版本文件 " + Model.Utils.Updater.OldName + ".exe");
                     File.Delete(Model.Utils.Updater.OldName + ".exe");
                 }
 
                 if (File.Exists(Model.Utils.Updater.NewName))
                 {
-                    MessageBox.Show("Rename .new to .new.exe");
+                    Model.Utils.Log4Net.WriteLog("重命名 .new 到 .new.exe");
                     File.Move(Model.Utils.Updater.NewName, Model.Utils.Updater.NewName + ".exe");
-                    Process.Start(Model.Utils.Updater.NewName + ".exe");
+                    Process.Start(Model.Utils.Updater.NewName + ".exe"); //是否有可能导致一个没关一个又开了呢？
                     return true;
                 }
                 else
@@ -93,35 +106,40 @@ namespace Drcom_Dialer
                 {
                     if (File.Exists(Model.Utils.Updater.NoExtName + ".exe"))
                     {
-                        MessageBox.Show("Rename .exe to .old.exe");
+                        Model.Utils.Log4Net.WriteLog("重命名 .exe 到 .old.exe");
                         File.Move(Model.Utils.Updater.NoExtName + ".exe", Model.Utils.Updater.OldName + ".exe");
                         Process.Start(Model.Utils.Updater.OldName + ".exe");
                         return true;
                     }
                     else
                     {
-                        // Exception
-                        return false;
+                        Model.Utils.Log4Net.WriteLog("未找到 .exe，尝试直接使用 .new.exe");
+                        File.Copy(Model.Utils.Updater.NewName + ".exe", Model.Utils.Updater.NoExtName + ".exe");
+                        Process.Start(Model.Utils.Updater.NoExtName + ".exe");
+                        return true;
                     }
                 }
                 else if (suff == Model.Utils.Updater.OldSuffix) // .old.exe
                 {
                     if (File.Exists(Model.Utils.Updater.NewName + ".exe"))
                     {
-                        MessageBox.Show("Rename .new.exe to .exe");
+                        Model.Utils.Log4Net.WriteLog("重命名 .new.exe to .exe");
                         File.Move(Model.Utils.Updater.NewName + ".exe", Model.Utils.Updater.NoExtName + ".exe");
                         Process.Start(Model.Utils.Updater.NoExtName + ".exe");
                         return true;
                     }
                     else
                     {
-                        // Exception
-                        return false;
+                        Model.Utils.Log4Net.WriteLog("未找到 .new.exe，尝试还原");
+                        File.Copy(Model.Utils.Updater.OldName + ".exe", Model.Utils.Updater.NoExtName + ".exe");
+                        Process.Start(Model.Utils.Updater.NoExtName + ".exe");
+                        return true;
                     }
                 }
                 else
                 {
-                    // Exception
+                    Model.Utils.Log4Net.WriteLog("无法识别的文件后缀");
+                    // 正常流程
                     return false;
                 }
             }
