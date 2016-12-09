b5b1fc05670d34010df1b3d5b9bd9f15d064a460
Wingkou Loeng <961574992@qq.com>
2016-12-09 20:08:35 +0800

    防止多启

--- Drcom-Dialer/App.xaml.cs
+++ Drcom-Dialer/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using Drcom_Dialer.Properties;
 
 namespace Drcom_Dialer
 {
@@ -13,5 +15,18 @@ namespace Drcom_Dialer
     /// </summary>
     public partial class App : Application
     {
+        private static readonly Mutex singleInstanceWatcher;
+        private static readonly bool createdNew;
+
+        static App()
+        {
+            string mutexName = Drcom_Dialer.Properties.Resources.ProgramTitle + "Mutex";
+            singleInstanceWatcher = new Mutex(false, mutexName, out createdNew);
+            if (!createdNew)
+            {
+                MessageBox.Show("程序已经运行!", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                Environment.Exit(-1);
+            }
+        }
     }
 }
