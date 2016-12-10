f2d56d1248eb90b1ebb1fd9cf84457a6ed4c918f
lindexi_gd <lindexi_gd@163.com>
2016-12-10 09:26:31 +0800

    修改到期时间

--- Drcom-Dialer/ViewModel/AccountInfoModel.cs
+++ Drcom-Dialer/ViewModel/AccountInfoModel.cs
@@ -73,9 +73,21 @@ namespace Drcom_Dialer.ViewModel
                     var temp = info.note;
                     Username = temp.welcome;
                     Money = temp.leftmoeny;
-                    Overdata = temp.overdate;
+                    ////到期时间有 "；到期时间" 于是去掉";"
+                    Overdata = ConvertOverdataStrToDate(temp.overdate); //temp.overdate.Replace("；","").Replace(";","");
                 }
             }).Start();
+        }
+
+        private static string ConvertOverdataStrToDate(string overdata)
+        {
+            //overdata = "；到期时间 2017.6.20";
+            int n = 20;//时间开头是  201*年x月
+            n = overdata.IndexOf(n.ToString());
+            //第一个 20 
+            //最后一个可能 20日  2017.1.20 不能拿最后一个
+            //Console.WriteLine(overdata.Substring(n));
+            return overdata.Substring(n);
         }
     }
 }
