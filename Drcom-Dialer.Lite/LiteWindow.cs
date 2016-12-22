9f639921c6951d94dab5e9821dbb29339f318c0f
Wingkou Loeng <961574992@qq.com>
2016-12-23 01:16:18 +0800

    Lite版基础界面、功能

--- Drcom-Dialer.Lite/LiteWindow.cs
+++ Drcom-Dialer.Lite/LiteWindow.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Drcom_Dialer.Model;
+using Drcom_Dialer.Model.Utils;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,66 @@ namespace Drcom_Dialer.Lite
         public LiteWindow()
         {
             InitializeComponent();
+
+            RegisterEventHandler();
+        }
+
+        private void RegisterEventHandler()
+        {
+            PPPoE.PPPoEDialSuccessEvent += (s, e) =>
+            {
+                statusLabel.Text = "拨号成功";
+                dialBtn.Invoke((MethodInvoker)(() => { Text = "断开"; }));
+                DialerConfig.SaveConfig();
+            };
+            PPPoE.PPPoEHangupSuccessEvent += (s, e) =>
+            {
+                statusLabel.Text = "断开成功";
+                dialBtn.Invoke((MethodInvoker)(() => { Text = "拨号"; }));
+            };
+            PPPoE.PPPoEDialFailEvent += (s, e) =>
+            {
+                statusLabel.Text = "拨号失败";
+            };
+            PPPoE.PPPoEHangupFailEvent += (s, e) =>
+            {
+                statusLabel.Text = "断开失败";
+            };
+            HeartBeatProxy.HeartbeatExited += (s, code) =>
+            {
+                statusLabel.Text = $"心跳停止({code})";
+            };
+            NetworkCheck.InnerNetworkCheckFailed += (s, e) => { };
+            NetworkCheck.OuterNetworkCheckFailed += (s, e) => { };
+            NetworkCheck.OuterNetworkCheckSuccessed += (s, e) => { };
+        }
+
+        private async void dialBtn_Click(object sender, EventArgs e)
+        {
+            await Task.Factory.StartNew(() =>
+            {
+                PPPoE.Dial("\r\n" + userText.Text, paswText.Text);
+            });
+        }
+
+        private void settingBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void aboutBtn_Click(object sender, EventArgs e)
+        {
+            new AboutBox().ShowDialog();
+        }
+
+        private void remPaswCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void autoLoginCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
